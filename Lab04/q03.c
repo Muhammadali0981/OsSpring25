8dfa074520bbd6604f28b0d5e9411ccf466d8071
Muhammad Ali <muhammadali030605@gmail.com>
2025-03-05 08:10:34 +0500

    Create q03.c

---
+++ Lab04/q03.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+int main() {
+    printf("Current Process ID (PID): %d\n", getpid());
+    printf("Parent Process ID (PPID): %d\n", getppid());
+    printf("User ID (UID): %d\n", getuid());
+    
+    return 0;
+}
