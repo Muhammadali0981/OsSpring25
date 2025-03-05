1a205938bcd032aa49ec7e2e1359b41e4740bf33
Muhammad Ali <muhammadali030605@gmail.com>
2025-03-05 08:10:10 +0500

    Create q02.c

---
+++ Lab04/q02.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+int main() {
+    pid_t pid = fork();
+
+    if (pid == 0) {
+        // Child process
+        for (int i = 0; i < 100; i++)
+            printf("I am a child process\n");
+    } else {
+        // Parent process
+        for (int i = 0; i < 100; i++)
+            printf("I am a parent process\n");
+    }
+
+    return 0;
+}
