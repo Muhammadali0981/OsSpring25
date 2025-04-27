1196369d0b344c8b033ba527cbf439628329efac
Muhammadali0981 <muahmmadali030605@gamil.com>
2025-04-27 19:30:01 +0500

    lab 6

---
+++ Lab06/q02_c.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+
+#define FIFO_FILE "file"
+
+int main() {
+	int fd;
+	char buffer[BUFSIZ];
+	ssize_t num_bytes;
+
+	fd = open(FIFO_FILE, O_RDONLY);
+	if (fd == -1) {
+		perror("open");
+		exit(EXIT_FAILURE);
+	}
+
+	while (1) {
+		num_bytes = read(fd, buffer, BUFSIZ);
+		if (num_bytes == -1) {
+			perror("read");
+			exit(EXIT_FAILURE);
+		} else if (num_bytes == 0) {
+			printf("exiting\n");
+			exit(EXIT_SUCCESS);
+		}
+
+		buffer[num_bytes] = '\0';
+		printf("received message: %s\n", buffer);
+	}
+	
+	close(fd);
+
+	return 0;
+}
